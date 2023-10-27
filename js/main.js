bf523e85de47d09edcef19c95c297748c31cd331
aobarbieri <aobarbieri@gmail.com>
2023-10-27 10:43:59 -0600

    Add updateStats

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-console.log('js:loaded')
-
 /*----- constants -----*/
 /* Initial data states 
 animation names
 image assets paths
 */
 
-const state = {
-    
+const INIT_STATE = {
+	boredom: 0,
+	hunger: 0,
+	sleepiness: 0,
 }
 /*----- state variables -----*/
 //state is the data that will change while the game is running
 
+let state
 
-
-let boredom
-let hunger
-let sleepiness
+// let boredom
+// let hunger
+// let sleepiness
 
 // Icebox features (age cycle for tama)
 let age
@@ -26,8 +26,8 @@ let timer // setInterval id
 let interval // count of cycles
 
 /*----- cached elements  -----*/
-const boredomStaEl = document.querySelector('#boredom-stat')
-const hungerStatEl= document.querySelector('#hunger-stat')
+const boredomStatEl = document.querySelector('#boredom-stat')
+const hungerStatEl = document.querySelector('#hunger-stat')
 const sleepyStatEl = document.querySelector('#sleepiness-stat')
 
 // TODO: add cache for game message string once added to game
@@ -38,28 +38,57 @@ const gameBtnEls = document.querySelectorAll('#controller button')
 
 /*----- event listeners -----*/
 
-
 /*----- functions -----*/
 init() //start the game when js loads
 
 function init() {
-    
-    age = 0;
-    cycles = 0;
-    
-    interval = 5000;
-    timer = setInterval(runGame, interval);
+	state = { ...INIT_STATE }
 
-    console.log('game has started')
+	age = 0
+	cycles = 0
 
-    // it will also call render () -> dom updates (trigger all render helper function -> updating stats)
-    render()
+	interval = 5000
+	timer = setInterval(runGame, interval)
+
+	console.log('game has started')
+
+	// it will also call render () -> dom updates (trigger all render helper function -> updating stats)
+	render()
 }
 
 function runGame() {
-    console.log('game is running')
+	updateStats()
+	render()
 }
 
 function render() {
-    console.log('rendering game')
+	// any features which might update the dom (the ui) -> will be called by render
+	renderStats()
+}
+
+function renderStats() {
+	boredomStatEl.textContent = state.boredom
+	hungerStatEl.textContent = state.hunger
+	sleepyStatEl.textContent = state.sleepiness
+}
+
+function updateStats() {
+	for (key in state) {
+		// let randomAmount = Math.floor(Math.random() * 3)
+		// let currentValue = state[key]
+		// state[key] = currentValue + randomAmount
+        // console.log(key, state[key], randomAmount)
+        
+        updateStat(key, Math.floor(Math.random()*3))
+	}
+}
+
+function updateStat(stat, value) {
+
+    if (state[stat] + value >= 0) {
+        stat[state] += value
+    } else { 
+        state[stat] = 0
+    }
+
 }
